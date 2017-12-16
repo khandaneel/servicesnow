f7eaf599cbf5868bf7a39b86ecd89780dffd28e6
madhav <khanda.neel@gmail.com>
2017-12-17 05:10:01 +0545

    new chnages

--- index.php
+++ index.php
@@ -6,3 +6,19 @@ if(!empty($USERPROFILE['profile_avatar'])) { $s4=1;} if(!empty($USERPROFILE['las
 $tscore=($s1+$s2+$s3+$s4+$s5+5)*10;
 $tscoren=($tscore*2.51);
  ?>
+
+
+</<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>Page Title</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" type="text/css" media="screen" href="main.css" />
+    <script src="main.js"></script>
+</head>
+<body>
+    My app demo
+</body>
+</html>
